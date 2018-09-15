3c5cb7520479ea896226694438a8b7f9902cca7a
arj0819 <aaronandsydney0819@gmail.com>
2018-09-15 15:16:26 -0500

    Newfiles: basicFunction.cpp and basicFunction.h

---
+++ ch02/basicFunction.cpp
@@ -0,0 +1,17 @@
+//basicFunction.cpp by Aaron Johnson
+#include <cstdio>
+#include "basicFunction.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    puts("This is main()");
+    func();
+
+    return 0;
+}
+
+void func() {
+    puts("This is func()");
+}
---
+++ ch02/basicFunction.h
@@ -0,0 +1,8 @@
+//basicFunction.h by Aaron Johnson
+
+#ifndef BASIC_FUNCTION_H
+#define BASIC_FUNCTION_H
+
+void func();
+
+#endif // BASIC_FUNCTION_H
