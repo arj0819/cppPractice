ff14b10f007f463b5ecc403c2adef6b51d160eea
arj0819 <aaronandsydney0819@gmail.com>
2018-09-14 23:41:43 -0500

    Newfile: switchCase.cpp

---
+++ ch01/switchCase.cpp
@@ -0,0 +1,36 @@
+//switchCase.cpp by Aaron Johnson
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    const int iONE = 1;
+    const int iTWO = 2;
+    const int iTWENTY = 20;
+    const int iTHREE = 3;
+    const int iFOUR = 4;
+    
+    int x = 2;
+
+    switch(x) {
+        case iONE:
+            puts("one");
+            break;
+        case iTWO:
+        case iTWENTY:
+            puts("two or twenty");
+            break;
+        case iTHREE:
+            puts("three");
+            break;
+        case iFOUR:
+            puts("four");
+            break;
+
+        default:
+            puts("default");
+            break;
+    }
+
+    return 0;
+}
