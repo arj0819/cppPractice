25167ba7b3079e7d569f9225bc34a93d9cf5568d
arj0819 <aaronandsydney0819@gmail.com>
2018-09-14 21:08:55 -0500

    Adds examples.cpp

---
+++ ch01/examples.cpp
@@ -0,0 +1,14 @@
+// examples.cpp by Aaron Johnson
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    int x;
+    x = 42;
+    printf("x is %d\n", x);
+    printf("x is %d\n", x = 50);
+
+    return 0;
+}
