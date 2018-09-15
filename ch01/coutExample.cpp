7b848271f0dbde938c252a2fadf70d894aca059e
arj0819 <aaronandsydney0819@gmail.com>
2018-09-15 01:10:51 -0500

    Build: Adds information about cout to coutExample.cpp

--- ch01/coutExample.cpp
+++ ch01/coutExample.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+    /*
+        cout is a C++ Class that overloads the bitwise left shift
+        operater (<<) to provide syntactic convenience when 
+        outputting information to standard out. However, using cout
+        balloons the size of the file because it requires the entire
+        cout class to print to standard out. 
+    */
     cout << "Hello, World! |" << 2 * 7 << "| another String" << endl;
 
     return 0;
