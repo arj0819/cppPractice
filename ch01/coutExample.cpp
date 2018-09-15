d2f3d9fc57368c21ee349b7f92ab862d8bf743d7
arj0819 <aaronandsydney0819@gmail.com>
2018-09-15 01:07:51 -0500

    Newfile: coutExample.cpp

---
+++ ch01/coutExample.cpp
@@ -0,0 +1,10 @@
+//coutExample.cpp by Aaron Johnson
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    cout << "Hello, World! |" << 2 * 7 << "| another String" << endl;
+
+    return 0;
+}
