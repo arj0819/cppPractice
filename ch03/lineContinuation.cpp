939d351d9a52d02c7e8939bc056cb32f26268b0c
arj0819 <aaronandsydney0819@gmail.com>
2018-09-16 21:47:30 -0500

    Newfile: lineContinuation.cpp

---
+++ ch03/lineContinuation.cpp
@@ -0,0 +1,29 @@
+//lineContinuation.cpp by Aaron Johnson
+
+#include <cstdio>
+using namespace std;
+
+/*
+When using preprocessor directives, you can use the \
+to indicate to the preprocessor that the directive
+continues on the next line. This makes long directives
+easier to read.
+*/
+
+#define SWAP(a,b) do {          \
+                        a ^= b; \
+                        b ^= a; \
+                        a ^= b; \
+                  } while (0)
+
+
+int main(int argc, char** argv) {
+
+    int x = 5;
+    int y = 10;
+    printf("x is %d, y is %d\n", x, y);
+    SWAP(x,y);
+    printf("x is %d, y is %d\n", x, y);
+
+    return 0;
+}
